5a24dae0d374d9479740c5ed7ffd5aab20d023a9
Florian TIAR <florian.tiar@theculturetrip.com>
2020-01-16 14:49:27 +0000

    Use BlockIcon component (Fix case where blocks with dashicons doesnt work)

--- plugins/02-toc/components/BlockButton.js
+++ plugins/02-toc/components/BlockButton.js
@@ -1,28 +1,31 @@
 const { getBlockType } = wp.blocks;
+const { BlockIcon } = wp.blockEditor;
 const { Button } = wp.components;
 const { select, dispatch, withSelect } = wp.data;
 
-import classnames from "classnames";
+import classnames from 'classnames';
 
-const BlockButton = ({ block, isBlockSelected }) => {
-  const blockType = getBlockType(block.name);
-  return (
-    <Button
-      className={classnames("editor-block-navigation__item-button", {
-        "is-selected": isBlockSelected
-      })}
-      onClick={() => dispatch("core/editor").selectBlock(block.clientId)}
-    >
-      <span className="block-editor-block-icon has-colors">{blockType.icon.src}</span>
-      {blockType.title}
-    </Button>
-  );
+const BlockButton = ( { block, isBlockSelected } ) => {
+	const blockType = getBlockType( block.name );
+	return (
+		<Button
+			className={ classnames( 'components-button block-editor-block-navigation__item-button', {
+				'is-selected': isBlockSelected,
+			} ) }
+			onClick={ () => dispatch( 'core/editor' ).selectBlock( block.clientId ) }
+		>
+			<BlockIcon icon={ blockType.icon } />
+			<span className={ 'block-editor-block-navigation__item-button__block-label' }>
+				{ blockType.title }
+			</span>
+		</Button>
+	);
 };
 
 // export default BlockButton;
-export default withSelect((select, ownProps) => {
-  const { clientId } = ownProps.block;
-  return {
-    isBlockSelected: select("core/editor").isBlockSelected(clientId)
-  };
-})(BlockButton);
+export default withSelect( ( select, ownProps ) => {
+	const { clientId } = ownProps.block;
+	return {
+		isBlockSelected: select( 'core/editor' ).isBlockSelected( clientId ),
+	};
+} )( BlockButton );
